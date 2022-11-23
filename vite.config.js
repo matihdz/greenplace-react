18916bd682e46b0adf277624f8f3166a7990fcaa
matihdz <matias.iturrieta.hdz@gmail.com>
2022-11-22 23:12:13 -0300

    manifest

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import vitePluginRequire from "vite-plugin-require";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vitePluginRequire.default()],
+  plugins: [
+    react(),
+    vitePluginRequire.default(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        short_name: "Greenplace",
+        name: "Greenplace",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff",
+      },
+    }),
+  ],
   base: "./react-greenplace/",
   build: {
     manifest: true,
